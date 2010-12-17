f5df025f1907ade9d42ee017f36d1c4245380666
Jochen Sprickerhof <jochen@sprickerhof.de>
2010-12-17 15:23:45 +0100

    added third axis of the Cyborg Evo to limit the speed
    added new parameter max_vel_x and max_rotational_vel to set a hard speed
    limit.

--- kurt_teleop/src/kurt_teleop_cyborgevo.cpp
+++ kurt_teleop/src/kurt_teleop_cyborgevo.cpp
@@ -2,15 +2,14 @@
 #include <geometry_msgs/Twist.h>
 #include <joy/Joy.h>
 
-int linear, angular;
-double l_scale, a_scale;
+double max_vel_x, max_rotational_vel;
 ros::Publisher vel_pub;
 
 void cyborgevoCallback(const joy::Joy::ConstPtr& joy)
 {
   geometry_msgs::Twist vel;
-  vel.angular.x = a_scale * joy->axes[angular];
-  vel.linear.x = l_scale * joy->axes[linear];
+  vel.linear.x = max_vel_x * (joy->axes[2] + 1) / 2 * joy->axes[1];
+  vel.angular.x = max_rotational_vel * (joy->axes[2] + 1) / 2 * joy->axes[0];
   vel_pub.publish(vel);
 }
 
@@ -21,10 +20,8 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::NodeHandle nh_ns("~");
 
-  nh_ns.param("axis_linear", linear, 1);
-  nh_ns.param("axis_angular", angular, 0);
-  nh_ns.param("scale_angular", a_scale, 2.0);
-  nh_ns.param("scale_linear", l_scale, 2.0);
+  nh_ns.param("max_vel_x", max_vel_x, 1.5);
+  nh_ns.param("max_rotational_vel", max_rotational_vel, 3.0);
 
   vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   ros::Subscriber cyborgevo_sub = nh.subscribe("joy", 10, cyborgevoCallback);
