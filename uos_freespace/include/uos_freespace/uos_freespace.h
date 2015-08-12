5f57e790e15e0233490b8a8ee642b7b3e83e9108
Peter Purnyn <mpurnyn@gmail.com>
2015-08-12 16:11:08 +0200

    Moddified uos_freespace to be object oriented
    Now Object based instead of function based.
    Added better comments in English.
    Improved variable/function names.
    Removed unnessary code.
    Changed naming conventions to be better in line with ROS:
    Class variables use a trailing underscore, methods and local variables are camelcase.

---
+++ uos_freespace/include/uos_freespace/uos_freespace.h
@@ -0,0 +1,38 @@
+/*
+ * uos_freespace
+ * Author: Marat "Peter" Purnyn
+ * Created 11/8/2015 (based on earlier work uos-freespace.cc)
+ */
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include "tf/transform_listener.h"
+
+class FreeSpace {
+	public:
+			FreeSpace();
+	private:
+		//variables
+			double max_vel_x_, max_rotational_vel_; //max velocity of robot
+			int turn_state_; 	//if robot gets too close to an object in front, it switches to turn mode
+								//it keeps turning until there is no more obstacle and switches out of turn mode.
+			int scanner_orientation_; //0 for initial, 1 for right side up, -1 for upside-down laserscanner
+		//node
+			ros::NodeHandle nh_;
+			ros::NodeHandle private_nh_;
+		//publishers
+			ros::Publisher vel_pub_;
+		//subscribers
+			ros::Subscriber laser_sub_;
+		//others
+			tf::TransformListener tf_;
+			
+		//functions
+			void autonomousBehaviour(const sensor_msgs::LaserScan::ConstPtr &laserscan);
+			double calcFreespace(const sensor_msgs::LaserScan::ConstPtr &laserscan);
+			int isInvertedScannerCheck(const sensor_msgs::LaserScan::ConstPtr &laserscan);
+			int checkRange(
+				const sensor_msgs::LaserScan::ConstPtr &laserscan, 
+				double xregion, double yregion,
+				int *indexToObstacle, 
+				double *distanceToObstacle);	
+};
