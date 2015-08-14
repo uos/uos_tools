5a7490ffb77d3c788f73bc03ae01ca3d14b6b15a
Peter Purnyn <mpurnyn@gmail.com>
2015-08-14 14:15:20 +0200

    Fixed stuck-in-corner bug and related bugs.
    turn_state_=1 now turns in one direction until it sees free space.
    This fixes the bug where the robot gets stuck in a corner and can't decide if it should turn left or right.
    Now ignores indexOfObstacle = -1 when turning right.
    Scanner_orientation now modifies turning direction right before publishing.
    Re-added if statement to ignore ranges[i] values below 0.1m in CheckRange() and calcFreeSpace() to discard phantom readings of 0m.
    Added debug messages to test turn_state_.

--- uos_freespace/src/uos_freespace.cpp
+++ uos_freespace/src/uos_freespace.cpp
@@ -18,10 +18,11 @@
 FreeSpace::FreeSpace():
 	turn_state_(0),
 	scanner_orientation_(0),
+	turn_omega_(0),
 	private_nh_("~")
 {
 	//sets the limits for the speed of the navigation
-	private_nh_.param("max_vel_x_", max_vel_x_, 0.3);
+	private_nh_.param("max_vel_x_", max_vel_x_, 0.5);
 	private_nh_.param("max_rotational_vel_", max_rotational_vel_, 0.3);
 	
 	turn_state_ = 0; 
@@ -42,21 +43,23 @@ double FreeSpace::calcFreespace(const sensor_msgs::LaserScan::ConstPtr &lasersca
 	double alpha = 0.0;
 	
 	for (i = 0; i < laserscan->ranges.size(); i++) {
-		phi = laserscan->angle_min + i * laserscan->angle_increment;
-		
-		//the arg of cos should not be smaller than phi/2 otherwise 
-		//the robot will be unstable when obstacles are close to its side. 
-		//The exponential function is used to weight the range information.
-		//The farther an object is the larger its weight.
-		orientationWeight = cos(phi/2) * 1.0/(1.0+exp(-(laserscan->ranges[i]-1)/0.3));
-		
-		sinSum += sin(phi) * orientationWeight;
-		cosSum += cos(phi) * orientationWeight;
+		if(laserscan->ranges[i] > 0.1){
+			phi = laserscan->angle_min + i * laserscan->angle_increment;
+			
+			//the arg of cos should not be smaller than phi/2 otherwise 
+			//the robot will be unstable when obstacles are close to its side. 
+			//The exponential function is used to weight the range information.
+			//The farther an object is the larger its weight.
+			
+			orientationWeight = cos(phi/2) * 1.0/(1.0+exp(-(laserscan->ranges[i]-1)/0.5));
+			sinSum += sin(phi) * orientationWeight;
+			cosSum += cos(phi) * orientationWeight;
+		}
 	}
 	if (fabs(cosSum) > EPSILON){
 		alpha = atan2(sinSum, cosSum);
 	}
-	return scanner_orientation_*alpha;
+	return alpha;
 }
 
 int FreeSpace::isInvertedScannerCheck(const sensor_msgs::LaserScan::ConstPtr &laserscan){
@@ -110,33 +113,39 @@ int FreeSpace::checkRange(
 	
 	//calculate the number of laserpoints in the deadzone
 	int deadzonePoints = int(floor((((LASERSCAN_DEADZONE)*(M_PI/180))/laserscan->angle_increment)));
-
+	ROS_WARN("deadzonepoints :%d", deadzonePoints);
 	//The laser scanner detects part of the robot frame.
 	//ignore laserpoints in deadzone on the left and right of the scan
 	for (i = fabs(deadzonePoints); i < (laserscan->ranges.size() - fabs(deadzonePoints)); i++) {
-		double x = laserscan->ranges[i] * cos(laserscan->angle_min + i * laserscan->angle_increment);
-		double y = laserscan->ranges[i] * sin(laserscan->angle_min + i * laserscan->angle_increment);
-		if ((x < xregion) && (fabs(y) < (yregion / 2))) { 
-			// Is there something in the region in front of the robot?
-			if (*distanceToObstacle > x) {
-			//Found obstacle, record distance and index
-			  *distanceToObstacle = x;	
-			  *indexOfObstacle = i;
-			}
-		}
-		if ((fabs(y) < (yregion / 2))) { 
-			//Is there something ahead of the robot?
-			if (generalDistance > x) {
-			//Set general distance to the closest object infront of the robot
-			  generalDistance = x;		   
+		if(laserscan->ranges[i] > 0.1){
+			double x = laserscan->ranges[i] * cos(laserscan->angle_min + i * laserscan->angle_increment);
+			double y = laserscan->ranges[i] * sin(laserscan->angle_min + i * laserscan->angle_increment);
+			if ((x < xregion) && (fabs(y) < (yregion / 2))) { 
+				// Is there something in the region in front of the robot?
+				if (*distanceToObstacle > x) {
+				//Found obstacle, record distance and index
+				  *distanceToObstacle = x;	
+				  *indexOfObstacle = i;
+				  ROS_WARN("set index to obstacle %d", i);
+				}
+			} //is there something in the generel direction?
+			if ((x > xregion) && (fabs(y) < (yregion / 2))) { 
+				//Is there something ahead of the robot?
+				if (generalDistance > x) {
+				//Set general distance to the closest object infront of the robot
+				  generalDistance = x;	
+				  //ROS_WARN("set general distance");	   
+				}
 			}
 		}
 	}
-
+	
 	if (*indexOfObstacle == -1){ 
 		//There is no obstacle, so reset the distance.
 		*distanceToObstacle = generalDistance;
 	}
+	ROS_WARN("distance to obstacle :%f", *distanceToObstacle);
+	ROS_WARN(" index obstacle :%d", *indexOfObstacle);
 	return 0;		
 }
 
@@ -152,7 +161,7 @@ void FreeSpace::autonomousBehaviour(const sensor_msgs::LaserScan::ConstPtr &lase
 	// define the size
 	double xregion = 0.5; // x is in forward
 	double yregion = 0.5;  // y is left
-	double DTOStopTurning = 1; // defines when to start to turn into freespace
+	double DTOStopTurning = 1.0; // defines when to start to turn into freespace
 	double DTOStartTurning = 0.5; // defines when to start to turn away form an obstacle
 	int indexOfObstacle = -1;
 	double frontMaxDistance = 0;
@@ -161,6 +170,8 @@ void FreeSpace::autonomousBehaviour(const sensor_msgs::LaserScan::ConstPtr &lase
 	double distanceToObstacle = INFINITY;
 	double omega = calcFreespace(laserscan); // find the freespace infront of the robot and return the angle to the direction of the freespace
 	double u = max_vel_x_;                 // define maximum velocity of the robot
+	
+	//ROS_WARN("behave maxpoints :%d", laserscan->ranges.size());
   
 	for(unsigned int i = 0; i < laserscan->ranges.size(); i++) {
 		if(laserscan->ranges[i] > frontMaxDistance) {
@@ -184,36 +195,40 @@ void FreeSpace::autonomousBehaviour(const sensor_msgs::LaserScan::ConstPtr &lase
 	// handle turning mode
 	// turn if obstacle is infront
 	if (turn_state_ == 1) {
+		ROS_WARN("turn_state: 1");
 		if ( (distanceToObstacle > DTOStopTurning)) {
 			turn_state_ = 0;                 // end turning
 		} 
 		else {
 			//turn slower if obstacle is infront
-			if (omega > 0.0) {
-				omega = max(max_rotational_vel_, (max_rotational_vel_ * (1 - (omega / (M_PI / 2)))));
-			}
-			else if (omega < 0.0) {
-				omega = min(-max_rotational_vel_, (max_rotational_vel_ * (-1 - (omega / (M_PI / 2)))));
-			}  
-	  }
+				ROS_WARN("turning away");
+		}  
 	  u = min(u, uTurning);          //set velocity to u if its not higher then the maximum velocity
 	}
 	// detect end of corridor use StopTurn val
 	if (((frontMaxDistance < DTOStopTurning) || (distanceToObstacle < DTOStartTurning)) && (turn_state_ == 0) ) {
 		// start turning
 		// turn away from obstacle
-		if (omega > 0.0) {
-			omega = min(omega,max_rotational_vel_); //omega should not exceed max_rotational_vel_
+		if (indexOfObstacle > laserscan->ranges.size() / 2) {
+			turn_omega_ = -max_rotational_vel_;
+			//omega = min(omega,max_rotational_vel_); //omega should not exceed max_rotational_vel_
+			ROS_WARN("turning_left");
 		}
-		else if (omega < 0.0) {
-			omega = max(omega,-max_rotational_vel_);
+		else if (indexOfObstacle <= laserscan->ranges.size() / 2 && indexOfObstacle >= 0) {
+			turn_omega_ = max_rotational_vel_;
+			
+			//omega = max(omega,-max_rotational_vel_);
+			ROS_WARN("turning_right");
 		}
 		turn_state_ = 1;
 		u = min(u, uTurning);
 	}
-
+	if (turn_state_ == 1){
+		omega = turn_omega_;
+	}
+	
 	geometry_msgs::Twist vel;
 	vel.linear.x = u;
-	vel.angular.z = omega;
+	vel.angular.z = scanner_orientation_ * omega;
 	vel_pub_.publish(vel);
 }
